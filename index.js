a78775a414f932abc1a653283da4982badbd35a8
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2026-02-02 22:29:17 +0000

    added all info that i had done from last week's challenge

---
+++ index.js
@@ -0,0 +1,40 @@
+let input = document.getElementById("input");
+let buttonEl = document.getElementById("add-button");
+let tasklist = document.getElementById("tasklist");
+
+let tasks = [] 
+// store tasks inside of an array
+
+buttonEl.addEventListener("click", onclickButton);
+// when button is clicked run the onclickbutton function (remember when clicking still 
+// does nothing)
+
+function onclickButton(){
+
+    let inputEl = input.value;
+    // whatever the user has typed store it in inputEl
+    let divItem = document.createElement("div");
+    // create a new html element (remember it does not exist yet)
+       divItem.textContent = inputEl;
+    // added users text inside the new div, so when user types, add eggs in the inputfield
+    // it then gets added
+
+    let deleteButton = document.createElement("button");
+    // creates a new html button element
+      deleteButton.textContent = "Delete";
+    //   adds visible text to the button
+
+    deleteButton.addEventListener("click", function(){
+        // when the button is click, run this function
+        tasklist.removeChild(divItem);
+        // remove the divitem this button belongs to
+
+
+    });
+
+       divItem.appendChild(deleteButton);
+    //    adds the delete button inside the task div
+       tasklist.appendChild(divItem)
+    //    appears on the page so user can see it
+    // also, has to be outside the function, otherwise it doesnt work!
+}
